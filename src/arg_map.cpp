485a65930a4861fdb08da685c164ae93c89a1ecc
Vladislav BALAGURA <balagura@cern.ch>
2020-03-22 23:47:34 +0100

    included missing header from not committed fun.club.hh directly to arg_map.cpp

--- src/arg_map.cpp
+++ src/arg_map.cpp
@@ -6,8 +6,15 @@
 #include <iostream>
 #include <cstdlib>  // for atoi()
 #include <Rcpp.h>
-#include "../inst/include/fun.club.h"
 using namespace Rcpp;
+
+struct Encoder {
+  std::map<std::string, int> m;                              // arg - int
+  std::map<int, std::pair<std::string, std::string> > m_inv; // int - (arg, printable_arg)
+  std::set<int> holes;                                       // int holes
+  std::set<int> values;                                      // int
+};
+typedef std::map<std::string, Encoder> Encoders;
 
 //'
 //'   String to integer encoder
